6d15e57225bbadcb405bc874f0ceca4d7ae8f95f
apurvjain28 <apurvjain28@gmail.com>
2023-01-16 13:27:08 -0500

    user-class-complete

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { User } from './models/User';
+
+const user = new User({ id: 1 });
+
+console.log(user.get('name'));
+
+user.on('change', () => {
+  console.log(user);
+});
+
+user.fetch();
